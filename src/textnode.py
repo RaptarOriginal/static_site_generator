686b1236ac05a37a4c8fe0b343d1958f663ceb8b
Raptar_Original <nickrappe@gmail.com>
2025-03-21 20:25:54 -0500

    create and populate .gitignore, main.sh, main.py and textnode.py. delete contents of the public directory.

---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINKS = "link"
+    IMAGES = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node):
+        if self == text_node:
+            return True
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
