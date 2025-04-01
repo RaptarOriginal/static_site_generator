24e933843f48f782770eda7d8d760e874ea13f23
Raptar_Original <nickrappe@gmail.com>
2025-03-31 20:11:37 -0500

    fixing formatting

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Replace "REPO_NAME" with the name of your GitHub repo
-python3 src/main.py "https://github.com/RaptarOriginal/static_site_generator"
+# Replace "static_site_generator" with the name of your GitHub repo
+python3 src/main.py "/static_site_generator/"
